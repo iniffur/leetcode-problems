60b149f97c9a1bbe3d7c6eb04107c5794666038c
iniffur <jamesruffini97@gmail.com>
2023-03-14 16:42:37 +0000

    Returns temperature array and catches error if given invalid value

--- 2469-Convert_The_Temperature/convertTemp.js
+++ 2469-Convert_The_Temperature/convertTemp.js
@@ -1,5 +1,6 @@
-const convertTemperatures = () => {
-  convertToKelvin();
+const convertTemperatures = (celsius) => {
+  checkTemperatureRange(celsius);
+  return [convertToKelvin(celsius), convertToFahrenheit(celsius)];
 };
 
 const convertToKelvin = (celsius) => {
@@ -10,6 +11,12 @@ const convertToKelvin = (celsius) => {
 const convertToFahrenheit = (celsius) => {
   let fahrenheitTemp = celsius * 1.8 + 32;
   return fahrenheitTemp;
+};
+
+const checkTemperatureRange = (celsius) => {
+  if (celsius < 0 || celsius > 1000) {
+    throw new Error("Error: Invalid Temperature");
+  }
 };
 
 module.exports = { convertTemperatures, convertToKelvin, convertToFahrenheit };
--- 2469-Convert_The_Temperature/convertTemp.test.js
+++ 2469-Convert_The_Temperature/convertTemp.test.js
@@ -12,4 +12,14 @@ describe("convertTemp", () => {
   it("can convert a temperature from celsius to Fahrenheit", () => {
     expect(convertToFahrenheit(36.5)).toEqual(97.7);
   });
+
+  it("Returns an array of both converted temperatures", () => {
+    expect(convertTemperatures(36.5)).toEqual([309.65, 97.7]);
+  });
+
+  it("Returns an error if temperature is not in range", () => {
+    expect(() => {
+      convertTemperatures(-36.5);
+    }).toThrow("Error: Invalid Temperature");
+  });
 });
